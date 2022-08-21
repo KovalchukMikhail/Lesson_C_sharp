2427897e9329249fec31e1720970e45e7d84e937
Kovalchuk_Mikhail <kovalchuckmikhail@yandex.ru>
2022-08-21 22:28:33 +0300

    HomeWork_9 added

---
+++ HomeWork_9/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+/*
+void ShowFromMToN(int numberM, int numberN)
+{
+    if (numberM < numberN)
+    {
+        if (numberM != numberN)
+        {
+            Console.Write($"{numberM} ");
+            ShowFromMToN(numberM + 1, numberN);
+        }
+    }
+    else if (numberM > numberN)
+    {
+        if (numberM != numberN)
+        {
+            Console.Write($"{numberM} ");
+            ShowFromMToN(numberM - 1, numberN);
+        }
+    }
+    else Console.Write($"{numberN} ");
+}
+
+Console.Write("Input number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+ShowFromMToN(numM, numN);
+*/
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
+int SumNumbers(int numberM, int numberN)
+{
+        if (numberM != numberN) return numberM + SumNumbers(numberM + 1, numberN);
+        else return numberN;
+}
+
+Console.Write("Input number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+if (numM < numN) Console.WriteLine($"Sum numbers from M to N: {SumNumbers(numM, numN)}");
+else Console.WriteLine($"Sum numbers from M to N: {SumNumbers(numN, numM)}");
+*/
+
+
+
+//  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int AckermannFunction (int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    if (numM > 0 && numN == 0) return AckermannFunction (numM - 1, 1);
+    else return AckermannFunction (numM - 1, AckermannFunction (numM, numN - 1));
+}
+
+Console.Write("Input number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The result counting Ackermann Function:{AckermannFunction(numM, numN)}");
---
+++ Lesson_9/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+/*
+void ShowNums(int n)
+{
+    if (n != 1) ShowNums(n - 1);
+
+    Console.Write(n + " ");
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ShowNums(num);
+*/
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SumOfDigits(int num)
+{
+    if (num != 0) return num % 10 + SumOfDigits(num / 10);
+    return 0;
+}
+
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of digits in {num} is {SumOfDigits(num)}");
+
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+
+
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+
+
